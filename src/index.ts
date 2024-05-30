ce4a4f530055fd81e11829da8521c9412674f2c1
lxs <858854440@qq.com>
2024-05-30 20:03:47 +0800

    init

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export {useFormScaffold} from './form-scaffold';
+export {componentNameEnum as CHILD} from './form-scaffold/utils/components-map'
 export type {FormScaffoldItem, FormScaffoldProps} from './form-scaffold';
 export type {UseFormScaffold} from './form-scaffold/types';
 export type {ComponentPropsMap} from './form-scaffold/utils/components-map';
